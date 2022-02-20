15c94723b476b4d2ea393da31129d610cdf7cfca
Sanaxo <91892197+Sanaxo@users.noreply.github.com>
2022-02-20 16:19:43 +0100

    Update video.js

--- video.js
+++ video.js
@@ -13,20 +13,20 @@ document.addEventListener('DOMContentLoaded', () => {
 	videoElement.poster = thumbnail;
 	videoElement.classList.add('Video');
 	videoWrapper.appendChild(videoElement);
-});
 
-function togglePlay() {
-	if (videoElement.paused || videoElement.ended) {
-		videoElement.play();
-	} else {
-		videoElement.pause();
+	function togglePlay() {
+		if (videoElement.paused || videoElement.ended) {
+			videoElement.play();
+		} else {
+			videoElement.pause();
+		}
 	}
-}
 
-function updatePlayButton() {
-	playbackIcons.forEach((icon) => icon.classList.toggle('hidden'));
-}
+	function updatePlayButton() {
+		playbackIcons.forEach((icon) => icon.classList.toggle('hidden'));
+	}
 
-playButton.addEventListener('click', togglePlay);
-videoElement.addEventListener('play', updatePlayButton);
-videoElement.addEventListener('pause', updatePlayButton);
+	playButton.addEventListener('click', togglePlay);
+	videoElement.addEventListener('play', updatePlayButton);
+	videoElement.addEventListener('pause', updatePlayButton);
+});
