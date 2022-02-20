295edb1211086deedcb2a5ef627645a489ca8e41
Sanaxo <91892197+Sanaxo@users.noreply.github.com>
2022-02-20 14:07:07 +0100

    install video.js

---
+++ video.js
@@ -0,0 +1,7 @@
+/** @format */
+
+const videoElement = document.querySelector('.Video');
+const videoSource = videoElement.getAttribute('data-video');
+console.log('Droggelbecher videoElement: ', videoElement);
+
+console.log('Droggelbecher videoSource: ', videoSource);
