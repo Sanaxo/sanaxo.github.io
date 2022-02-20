27bf83e89fd26df3194a27177312ffb0a4dd2cd1
Sanaxo <91892197+Sanaxo@users.noreply.github.com>
2022-02-20 14:27:43 +0100

    video1

--- video.js
+++ video.js
@@ -1,7 +1,16 @@
 /** @format */
 
-const videoElement = document.querySelector('.Video');
-const videoSource = videoElement.getAttribute('data-video');
-console.log('Droggelbecher videoElement: ', videoElement);
+document.addEventListener('load', () => {
+	const videoWrapper = document.querySelector('.Video');
+	const videoSource = videoElement.getAttribute('data-video');
+	const thumbnail = videoElement.getAttribute('data-thumbnail');
 
-console.log('Droggelbecher videoSource: ', videoSource);
+	console.log('Droggelbecher videoElement: ', videoElement);
+
+	console.log('Droggelbecher videoSource: ', videoSource);
+	const video = document.createElement('video');
+	console.log('Droggelbecher video: ', video);
+
+	video.src = videoSource;
+	video.poster = thumbnail;
+});
