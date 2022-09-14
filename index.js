fc50ad81065da9a50cf12451632c83ac9fb2197c
Sanaxo <91892197+Sanaxo@users.noreply.github.com>
2022-09-14 20:45:11 +0200

    mobile fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /** @format */
 document.addEventListener('DOMContentLoaded', () => {
   const megamenuItemElements = document.querySelectorAll('[data-js-menu-item]');
-  console.log('megamenuItemElements: ', megamenuItemElements);
+  const megaMenuElement = document.querySelector('[data-js-megamenu]');
 
   const selectActiveContent = (menuItems) => {
     let activeContent;
@@ -13,7 +13,6 @@ document.addEventListener('DOMContentLoaded', () => {
         const selector = `
           [data-container="${itemType}"]
         `;
-        console.log('selector: ', selector);
         activeContent = document.querySelector(selector);
       } else return;
     });
@@ -48,13 +47,51 @@ document.addEventListener('DOMContentLoaded', () => {
   };
 
   megamenuItemElements.forEach((megamenuItem) => {
-    megamenuItem.addEventListener('click', (event) => {
+    megamenuItem.addEventListener('click', () => {
       const aciveContent = selectActiveContent(megamenuItemElements);
-      console.log('aciveContent: ', aciveContent);
       closeContentContainer(megamenuItem, aciveContent);
       const contentToBeOpened = selectContentToBeOpened(megamenuItem);
-      console.log('contentToBeOpened: ', contentToBeOpened);
       openContentContainer(megamenuItem, contentToBeOpened);
     });
   });
+
+  const overlayElement = document.querySelector('[data-js-overlay]');
+  const navDrawerTriggerElement = document.querySelector('[data-js-nav-drawer]');
+
+  const showOverlay = () => {
+    overlayElement.classList.remove('-Hidden');
+    requestAnimationFrame(() => {
+      overlayElement.classList.add('-Show');
+    });
+  };
+
+  const hideOverlay = () => {
+    overlayElement.classList.remove('-Show');
+    requestAnimationFrame(() => {
+      overlayElement.classList.add('-Hidden');
+    });
+  };
+
+  const openNavDrawer = () => {
+    if (megaMenuElement.classList.contains('-Show')) return;
+    megaMenuElement.classList.add('-Show');
+    showOverlay();
+    navDrawerTriggerElement.removeEventListener('click', openNavDrawer);
+    requestAnimationFrame(() => {
+      navDrawerTriggerElement.addEventListener('click', closeNavDrawer);
+      overlayElement.addEventListener('click', closeNavDrawer);
+    });
+  };
+
+  const closeNavDrawer = () => {
+    navDrawerTriggerElement.removeEventListener('click', closeNavDrawer);
+    overlayElement.removeEventListener('click', closeNavDrawer);
+    megaMenuElement.classList.remove('-Show');
+    hideOverlay();
+    requestAnimationFrame(() => {
+      navDrawerTriggerElement.addEventListener('click', openNavDrawer);
+    });
+  };
+
+  navDrawerTriggerElement.addEventListener('click', openNavDrawer);
 });
